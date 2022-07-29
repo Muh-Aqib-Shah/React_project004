4c56d4d628fe12392c46d3e2616cf35ebe34c019
Muh-Aqib-Shah <109861813+Muh-Aqib-Shah@users.noreply.github.com>
2022-07-29 20:49:44 +0500

    some changes to button

--- src/Room.js
+++ src/Room.js
@@ -4,18 +4,32 @@ import "./Room.css"
 
 export let Room = () =>
 {
-    let [isLit,setLit]= useState(true)
+    let [isLit,setLit]= useState(false)
+    let[temp,settemp] = useState(22)
     function background() {
         setLit(!isLit)
 
+    }
+    function Temperatureadd() {
+        (settemp(++temp))     
+    }
+    function Temperaturesub() {
+            settemp(--temp)
     }
   
 
 
     return(
+        
         <div className={`room ${isLit? "Lit": "Dark"}`}>
-            <div className='chdir'>
-         <h1>The room is {isLit ? "Lit" : "Dark"} </h1>
+            <div className='fortemp' id='div'>
+             <h2>Temperature: {temp}<sup>o</sup>C</h2>
+             <button onClick={Temperatureadd} className='tempinc'>Increase</button>
+            <button onClick={Temperaturesub} className='tempdec'>Decrease</button>
+         </div>
+            <br/>
+            <div className='chdir' id='div'>
+         <h1>The room is {isLit? "Lit": "Dark"}</h1>
        <div className='box'><input type="checkbox" className='toggle' onClick={background}/></div>
        </div>
         </div>
